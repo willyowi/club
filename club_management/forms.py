ce6108a7b35a9c1e4eccea279cb46c84d1880a7d
Brian Sunday <brians931@gmail.com>
2020-01-30 13:35:35 +0300

    add custom forms to be used to add or change a user in the admin side

--- club_management/forms.py
+++ club_management/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from .models import *
 from django.contrib.auth.models import User	
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from .models import CustomUser
 
 
 class ClubForm(forms.ModelForm):
-
 
     class Meta:
         model = Club
@@ -15,7 +16,22 @@ class OfficialForm(forms.ModelForm):
     class Meta:
         model = Official
         fields = '__all__'
+
 class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
         fields = '__all__'
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model=CustomUser
+        fields=('email',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    
+    class Meta:
+        model = CustomUser
+        fields = ('email',)        
--- clubs/urls.py
+++ clubs/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 # from django.urls import path,include
-from django.conf.urls import url,include,path
+from django.conf.urls import url,include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views  as auth_views
@@ -27,10 +27,6 @@ urlpatterns = [
     url(r'', include('club_management.urls')),
     url(r'^logout/$', auth_views.logout, {"next_page": '/'}),
 
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
-
     # pa('admin/', admin.site.urls),
     # path('',include('club_management.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
@@ -38,3 +34,6 @@ if settings.DEBUG:
     # path('accounts/', include('registration.backends.simple.urls')),
     # url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
